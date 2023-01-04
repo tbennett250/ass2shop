e63e6192cfb0295cf4bbdb3408860403baff57a6
ben21005216 <ben21005216@C-010072>
2023-01-04 11:20:37 +0000

    init

---
+++ components/admin-nav.php
@@ -0,0 +1,18 @@
+<?php 
+
+?>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="#">Add Product</a>
+      <a class="nav-item nav-link" href="#">Manage Users </a>
+      <a class="nav-item nav-link" href="#">Manage Products</a>
+      <a class="nav-item nav-link disabled" href="#">Manage Catagories</a>
+    </div>
+  </div>
+</nav>
