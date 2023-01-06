bdf4e47b630e467739e41ed78f91529ffe953a9f
ben21005216 <ben21005216@C-010072>
2023-01-06 16:31:34 +0000

    EDIT USERS WIP

---
+++ components/edit-users-edit-user.php
@@ -0,0 +1,73 @@
+<?php
+require_once './inc/functions.php';
+$message = '';
+$UserToEdit = $controllers->members()->get('15');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    $firstname = InputProcessor::process_string($_POST['firstname'] ?? '');
+    $lastname = InputProcessor::process_string($_POST['lastname'] ?? '');
+    $email = InputProcessor::process_email($_POST['email']);
+
+    $valid = $firstname['valid'] && $lastname['valid'] && $email['valid'];
+
+    if($valid){
+         $args = [ 'firstname' => $firstname['value'],
+                    'lastname' => $lastname['value'],
+                    'email' => $email['value']];
+                    
+
+        $sql = "UPDATE users 
+                SET firstname = :firstname
+                    lastname = :lastname, 
+                    email = :email, 
+                WHERE id = :id;";
+
+
+       $process =  $controllers->members()->update($args);
+       redirect('manage-users');
+    }
+}
+?>
+
+
+
+<form method="POST" action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>" enctype="multipart/form-data">
+<section class="vh-100">
+<div class="container py-5 h-75">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div class="card shadow-2-strong" style="border-radius: 1rem;">
+              <div class="card-body p-5 text-center">
+    
+                <h3 class="mb-2">Edit User</h3>
+                <div class="form-outline mb-4">
+                    <label>Firstname</label>
+                  <input type="text" id="firstname" name="firstname" class="form-control form-control-lg" value="<?= $UserToEdit['firstname'] ?>" required value="<?= htmlspecialchars($name['value'] ?? '') ?>"/>
+                  <span class="text-danger"><?= $firstname['error'] ?? '' ?></span>
+                </div>
+                
+                <div class="form-outline mb-4">
+                <label>Last Name</label>
+                  <input type="text" id="lastname" name="lastname" class="form-control form-control-lg" value="<?= $UserToEdit['lastname'] ?>" required value="<?= htmlspecialchars($description['value'] ?? '') ?>"/>
+                  <span class="text-danger"><?= $lastname['error'] ?? '' ?></span>
+                </div>
+    
+    
+                <div class="form-outline mb-4">
+                <label>Email</label>
+                  <input type="text" id="email" name="email" class="form-control form-control-lg" value="<?= $UserToEdit['email'] ?>" required value="<?= htmlspecialchars($price['value'] ?? '') ?>"/>
+                  <span class="text-danger"><?= $email['error'] ?? '' ?></span>
+                </div>
+
+    
+                <button class="btn btn-primary btn-lg w-100 mb-4" type="submit">Edit Details</button>
+               
+                <?= isset($_GET['errmsg']) ? $message = $_GET['errmsg'] : '' ?>
+                <?= $message ? alert($message, 'danger') : '' ?>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
